c52b823aaf10924338d9424c0d4a77dc753c44d8
proger_papka <ziatdinov.eldar9@gmail.com>
2018-08-02 12:50:50 +0400

    created frontend and backend projects

---
+++ frontend/vue-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Hello World!</h2>
+    <login-form />
+    <success-message />
+  </div>
+</template>
+
+<script>
+  import LoginForm from "./LoginForm"
+  import SuccessMessage from "./SuccessMessage"
+
+  export default {
+    components: {
+      LoginForm,
+      SuccessMessage
+    }
+  }
+</script>
