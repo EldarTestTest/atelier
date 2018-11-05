d23c09f2d44d455f072833215f930fbfdd06080b
Eldar Ziatdinov <ziatdinov.eldar9@gmail.com>
2018-11-06 01:13:02 +0400

    добавлен страница с авторизацией

--- frontend/vue-admin/src/App.vue
+++ frontend/vue-admin/src/App.vue
@@ -1,16 +1,19 @@
 <template>
     <v-app>
-        <app-header></app-header>
+        <!--<app-header></app-header>-->
+        <sign-in></sign-in>
         <router-view/>
     </v-app>
 </template>
 
 <script>
     import AppHeader from '@/components/AppHeader'
+    import SignIn from '@/components/SignIn'
 
     export default {
         components: {
-            AppHeader
+            AppHeader,
+            SignIn
         }
     }
 
