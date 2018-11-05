e38dcd1fc7a7d5d93127a17dffc8c342b02d963c
Eldar Ziatdinov <ziatdinov.eldar9@gmail.com>
2018-11-05 22:07:13 +0400

    добавлен новый проект и первый простой шаблон

---
+++ frontend/vue-admin/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app>
+        <app-header></app-header>
+        <router-view/>
+    </v-app>
+</template>
+
+<script>
+    import AppHeader from '@/components/AppHeader'
+
+    export default {
+        components: {
+            AppHeader
+        }
+    }
+
+</script>
